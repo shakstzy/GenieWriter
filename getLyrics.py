429ab5649e85813f05b651f87b13f3f6e3fd2cda
Adithya Kumar <adithya.shak.kumar@gmail.com>
2022-10-30 19:05:45 -0700

    committed

--- getLyrics.py
+++ getLyrics.py
@@ -2,10 +2,10 @@
 
 
 
-1. Get Artist Name
-    a. Artist.search w name
-        - grab artist_id
-    b. track.search w artist_id, page_size = 20, s_track_rating = desc, format = JSON
-        - Grab track_id's
-    c. track.lyrics.get w track_id
-        - Grab lyrics from lyrics_body
+# 1. Get Artist Name
+#     a. Artist.search w name
+#         - grab artist_id
+#     b. track.search w artist_id, page_size = 20, s_track_rating = desc, format = JSON
+#         - Grab track_id's
+#     c. track.lyrics.get w track_id
+#         - Grab lyrics from lyrics_body
